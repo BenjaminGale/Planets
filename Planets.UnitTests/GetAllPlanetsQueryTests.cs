51d1d6efc3e962cb18756951713653f4a747cd9e
Benjamin Gale <benjaminiangale@gmail.com>
2023-03-16 10:50:24 +0000

    Add unit tests for use cases

---
+++ Planets.UnitTests/GetAllPlanetsQueryTests.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Planets.Application;
+using Planets.Application.UseCases.GetAllPlanets;
+using Planets.Domain;
+
+namespace Planets.UnitTests
+{
+    [TestClass]
+    public class GetAllPlanetsQueryTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void GivenAGetAllPlanetsQuery_WhenExecuted_ThenTheExpectedPlanetSummariesAreReturned(
+            IEnumerable<Planet> planets,
+            IEnumerable<PlanetSummaryDto> expectedResult)
+        {
+            var planetRepositoryMock = new Mock<IPlanetRepository>();
+            planetRepositoryMock
+                .Setup(r => r.GetAll())
+                .Returns(planets);
+
+            var sut = new GetAllPlanetsQuery(planetRepositoryMock.Object);
+            var result = sut.Execute();
+
+            result
+                .Should()
+                .BeEquivalentTo(expectedResult);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[]
+            {
+                Enumerable.Empty<Planet>(),
+                Enumerable.Empty<PlanetSummaryDto>()
+            };
+
+            yield return new object[]
+            {
+                new List<Planet> { Planet.Earth },
+                new List<PlanetSummaryDto> { new PlanetSummaryDto { Name = "Earth" } }
+            };
+
+            yield return new object[]
+            {
+                new List<Planet> { Planet.Uranus, Planet.Saturn },
+                new List<PlanetSummaryDto>
+                {
+                    new PlanetSummaryDto { Name = "Uranus" },
+                    new PlanetSummaryDto { Name = "Saturn" }
+                }
+            };
+
+            yield return new object[]
+            {
+                new List<Planet> { Planet.Jupiter, Planet.Venus, Planet.Mercury },
+                new List<PlanetSummaryDto>
+                {
+                    new PlanetSummaryDto { Name = "Jupiter" },
+                    new PlanetSummaryDto { Name = "Venus" },
+                    new PlanetSummaryDto { Name = "Mercury" }
+                }
+            };
+        }
+    }
+}
---
+++ Planets.UnitTests/GetPlanetQueryTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Planets.Application;
+using Planets.Application.UseCases.GetPlanet;
+using Planets.Domain;
+
+namespace Planets.UnitTests
+{
+    [TestClass]
+    public class GetPlanetQueryTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void GivenAGetPlanetsQuery_WhenExecuted_ThenTheExpectedPlanetDtoIsReturned(
+            string planetName,
+            Planet planet,
+            PlanetDto expectedResult)
+        {
+            var planetRepositoryMock = new Mock<IPlanetRepository>();
+            planetRepositoryMock
+                .Setup(r => r.Get(It.Is<string>(name => name == planetName)))
+                .Returns(planet);
+
+            var sut = new GetPlanetQuery(planetRepositoryMock.Object);
+            var result = sut.Execute(planetName);
+
+            result
+                .Should()
+                .BeEquivalentTo(expectedResult);
+        }
+
+        public static IEnumerable<object?[]> GetTestData()
+        {
+            yield return new object?[]
+            {
+                "Zebes",
+                null,
+                null
+            };
+
+            yield return new object?[]
+            {
+                "Earth",
+                Planet.Earth,
+                new PlanetDto
+                {
+                    Name = "Earth",
+                    Diameter = "12,756.00 km",
+                    DistanceFromSun = "149,600,000.00 km",
+                    Mass = "5.97x10^24 kg",
+                    Type = "Rocky"
+                }
+            };
+
+            yield return new object?[]
+            {
+                "Earth",
+                Planet.Jupiter,
+                new PlanetDto
+                {
+                    Name = "Jupiter",
+                    Diameter = "142,984.00 km",
+                    DistanceFromSun = "778,600,000.00 km",
+                    Mass = "1898x10^24 kg",
+                    Type = "GasGiant"
+                }
+            };
+        }
+    }
+}
